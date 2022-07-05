c9e681d8d97dfbfe884cbcad9498e68e94d07f37
Prashant Rai <95372360+Jpp14Prashant@users.noreply.github.com>
2022-07-05 15:40:23 +0545

    Update info.py

--- form/info.py
+++ form/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'django.form1@gmail.com'
-EMAIL_HOST_PASSWORD = 'jcdkbihrkvcclaru'
+EMAIL_HOST_USER = <domain email>
+EMAIL_HOST_PASSWORD = <email pw>
 EMAIL_PORT = 587
